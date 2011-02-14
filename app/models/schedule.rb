6dcc22a168896c67f8c4df1663c440d1f5154bea
Brad Phelan <bradphelan@xtargets.com>
2011-02-14 18:00:45 +1100

    Adding in schedule query,

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -16,4 +16,64 @@ class Schedule < ActiveRecord::Base
   # Room association
   belongs_to :room
   validates :room, :presence => true
+
+  # Schedule collision
+  validate do
+
+  end
+
+  DAYS = %w(monday tuesday wednesday thursday friday saturday sunday) 
+
+  validate do
+  end
+
+  def coliding_schedules_query
+    schedule = Schedule.arel_table
+    schedule[:monday].eq(true)
+    
+    # Do Times
+    
+    time_where = overlap_query \
+          schedule[:start_time],
+          schedule[:end_time],
+          start_time,
+          end_time
+
+    # Do Dates
+
+    date_where = overlap_query \
+        schedule[:start_date],
+        schedule[:end_date],
+        start_date,
+        end_date
+
+    # Do room
+    room_where = \
+      schedule[:room_id].eq(room.id)
+
+    days_where = DAYS.map(&:to_sym).map do |day|
+      if self[day]
+        schedule[day].eq(true)
+      else
+        nil
+      end
+    end.select do |p|
+      p
+    end.inject do |pred, d|
+      pred.or(d)
+    end
+
+
+    room_where.and(date_where).and(time_where).and(days_where)
+
+  end
+
+  private
+
+  def overlap_query field_lower, field_upper, p0, p1
+    field_lower.lt(p0).and(field_upper.gt(p0)).
+      or(  field_lower.gt(p0).and(field_upper.lt(p1))  ). 
+      or(  field_lower.lt(p1).and(field_upper.gt(p1))  ) 
+  end
+
 end
--- spec/models/schedule_spec.rb
+++ spec/models/schedule_spec.rb
@@ -1,5 +1,27 @@
 require 'spec_helper'
 
 describe Schedule do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @course_1 = Course.create
+    @course_2 = Course.create
+
+    @room_0 = Room.create :name => '3a'
+
+    @schedule = @course_1.schedules.build \
+      :start_time => '10:00',
+      :end_time   => '12:00',
+      :start_date => Time.now,
+      :end_date   => Time.now + 3.months,
+      :room       => @room_0,
+      :monday     => true,
+      :wednesday  => true,
+      :friday     => true
+  end
+
+  it "generates time overlap queries" do
+    require 'pp'
+    rel = @schedule.coliding_schedules_query
+    pp rel.to_sql
+  end
 end
