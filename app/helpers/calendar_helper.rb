e86e7d8b34b82716371cf1adafaf753827f374fb
Brad Phelan <bradphelan@xtargets.com>
2011-02-16 11:55:26 +1100

    The event link now links back to the schedule on the calendar

--- app/helpers/calendar_helper.rb
+++ app/helpers/calendar_helper.rb
@@ -18,7 +18,7 @@ module CalendarHelper
     # args is an argument hash containing :event, :day, and :options
     calendar event_calendar_opts do |args|
       event = args[:event]
-      %(<a href="/events/#{event.id}" title="#{h(event.name)}">#{h(event.name)}</a>)
+      link_to "#{h(event.name)}", "/admin/schedules/edit/#{event.schedule.id}"   
     end
   end
 end
