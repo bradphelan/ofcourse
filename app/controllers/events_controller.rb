07a326e27a3258be510e72047a7c1485c9526d3d
Brad Phelan <bradphelan@xtargets.com>
2011-02-16 13:35:57 +1100

    Added in URL link to the course from the event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,8 +4,15 @@ class EventsController < ApplicationController
   def index
     @events = Event.find(:all, :conditions => ["start_at >= '#{Time.at(params['start'].to_i).to_formatted_s(:db)}' and end_at <= '#{Time.at(params['end'].to_i).to_formatted_s(:db)}'"] )
     events = [] 
-    @events.each do |event|
-      events << {:id => event.id, :title => event.name, :description => event.schedule.course.description || "Some cool description here...", :start => "#{event.start_at.iso8601}", :end => "#{event.end_at.iso8601}", :allDay => event.all_day, :recurring => true}
+    events = @events.map do |event|
+      {
+        :id => event.id, 
+        :title => event.name, 
+        :description => event.schedule.course.description || "Some cool description here...", 
+        :start => "#{event.start_at.iso8601}", :end => "#{event.end_at.iso8601}", 
+        :allDay => event.all_day, :recurring => true,
+        :url => "admin/courses/edit/#{event.schedule.course.id}"
+      }
     end
     render :text => events.to_json
   end
